0c63bac7c1cb00256f6f8c973b433ad08a225f2e
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-04-04 22:00:52 +0800

    Create depth 2 config scenario expected files

--- spec/coffeebrew_jekyll_archives_spec.rb
+++ spec/coffeebrew_jekyll_archives_spec.rb
@@ -6,6 +6,7 @@ require_relative "./scenarios/default/context"
 require_relative "./scenarios/before_navigation/context"
 require_relative "./scenarios/after_navigation/context"
 require_relative "./scenarios/depth_one/context"
+require_relative "./scenarios/depth_two/context"
 
 SUCCESS_EXAMPLE = "generates a page for each depth of posts with correct navigation and link hierarchy"
 FAILURE_EXAMPLE = "raises Jekyll::Errors::InvalidConfigurationError"
@@ -66,6 +67,10 @@ RSpec.describe(Coffeebrew::Jekyll::Archives) do
     end
 
     include_context CONTEXT_DEPTH_ONE do
+      it_behaves_like SUCCESS_EXAMPLE
+    end
+
+    include_context CONTEXT_DEPTH_TWO do
       it_behaves_like SUCCESS_EXAMPLE
     end
   end
